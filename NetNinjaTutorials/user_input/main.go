902454ec2d3093320eebae0943bd38c438855e41
Michael <michaelgrant13@hotmail.co.uk>
2022-10-29 16:08:18 +0100

    promptOptions

--- NetNinjaTutorials/user_input/main.go
+++ NetNinjaTutorials/user_input/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -36,6 +37,41 @@ func promptOptions(b bill) {
 	opt, _ := getInput("Choose Options ( a - add item, s - save bill, t - add tip): ", reader)
 	fmt.Println(opt)
 
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number!")
+			promptOptions(b)
+		}
+
+		b.addItem(name, p)
+		fmt.Println("Item added - ", name, price)
+		promptOptions(b)
+
+	case "t":
+		tip, _ := getInput("Enter Tip amount: ", reader)
+
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number!")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+
+		fmt.Println("Tip added - ", tip)
+		promptOptions(b)
+
+	case "s":
+		fmt.Println("You chose to save the bill.", b)
+
+	default:
+		fmt.Println("That was not a valid option!")
+		promptOptions(b)
+	}
 }
 
 func main() {
