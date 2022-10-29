143a226e369f945c63ad8990ad81d72e02eb1c90
Michael <michaelgrant13@hotmail.co.uk>
2022-10-29 16:12:01 +0100

    save bill function

--- NetNinjaTutorials/user_input/bill.go
+++ NetNinjaTutorials/user_input/bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -47,4 +50,15 @@ func (b *bill) updateTip(tip float64) {
 // Update an Item in the Bill
 func (b *bill) addItem(name string, price float64) {
 	b.items[name] = price
+}
+
+func (b *bill) save() {
+	data := []byte(b.format())
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Bill was saved to file!")
+
 }
